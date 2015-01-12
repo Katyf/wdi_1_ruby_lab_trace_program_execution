b2b6cdf81b92b2681fe38bdaa8f47c680fc2416a
katyf <katy.feng.14@gmail.com>
2015-01-12 14:55:42 -0500

    Add comments to triangle sources.

--- lib/triangle/valid_triangle.rb
+++ lib/triangle/valid_triangle.rb
@@ -1,5 +1,10 @@
-# Returns true/false if a triangle is valid
+# Returns true if a triangle is valid ; false if the
+# triangle is invalid
 # as per the Triangle Inequality Theorem
+# Note: any side of a triangle is always shorter than
+# the sum of the other two sides
+# ? in a method indicates it will return T/F, doesn't
+# change the actual code
 def valid_triangle?(side_a, side_b, side_c)
   (side_a + side_b) > side_c &&
   (side_b + side_c) > side_a &&
